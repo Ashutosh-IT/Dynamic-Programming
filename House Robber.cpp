98600c2a9f85d9aac7b5e04e1a3bec00f4488f2e
Ashutosh-IT <127471296+Ashutosh-IT@users.noreply.github.com>
2024-05-22 00:37:28 +0530

    House Robber Leetcode Problem no. 198

---
+++ House Robber.cpp
@@ -0,0 +1,28 @@
+int rob(vector<int>& arr) {
+        int n=arr.size();
+        // its a DP problem similar to maximum subset sum with no two adjacent 
+        
+        // steps :
+        
+        // 1. create dp array
+        int dp[n];
+        
+        // 2. fill the some base cases in dp array
+        dp[0] = arr[0];
+        if(n>1)
+        dp[1] = max(dp[0],arr[1]);
+        if(n>2)
+        dp[2] = max(dp[0]+arr[2],dp[1]);
+        
+        // 3. fill the whole dp array using loop and formula
+        for(int i=3; i<n; i++){
+            
+            // 4. find the general formula
+            // dp[4] = max(dp[2]+arr[4],dp[3]) -- include 4th element or not
+            
+            dp[i] = max(dp[i-2]+arr[i],dp[i-1]);
+        }
+        
+        // return the last element of dp array
+        return dp[n-1];
+    }
